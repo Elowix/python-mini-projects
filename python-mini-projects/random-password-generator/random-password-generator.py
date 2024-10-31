c5f9bc547f02879d6e3c996a84c547462552a11c
Erfan-0 <shokranian1387@gmail.com>
2024-10-31 10:28:22 +0330

    Fixed random-password-generator bug

---
+++ python-mini-projects/random-password-generator/random-password-generator.py
@@ -0,0 +1,18 @@
+# TBA: add limitation for output
+
+import random  
+
+length = int(input('Enter the character password length: '))  
+
+letter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"  
+num = '1234567890'  
+symbols = '!@#$%^&*()_+=-'  
+
+all_characters = letter + num + symbols  
+
+password = ''  
+
+for i in range(length):  
+    password += random.choice(all_characters)
+
+print(password)
