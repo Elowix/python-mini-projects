1ed073348dbbec74942e85c7f42dd90aa4848213
𝐄𝐫𝐟𝐚𝐧 <127898295+Erfan-0@users.noreply.github.com>
2024-11-16 16:42:17 +0330

    update loterry.py

--- python-mini-projects/lottery/loterry.py
+++ python-mini-projects/lottery/loterry.py
@@ -1,31 +1,37 @@
 import random
-import time
 
-chbr = int(input('chand barande mikhay dashte bashi?: '))
+chbr = int(input('How many winners do you want to have?: '))
 if chbr == 0:
-       print('0 dalghak!? ')
-       time.sleep(2)
+       print('0, you idiot!?')
+       exit()
 
 
 plrname = None
 plrname_list = []
 while plrname != 'end':
-        plrname = input('esme ro vared kon: ')
+        plrname = input('Enter the name: ')
+
         if plrname == 'end':
-                print('shoma ghore ro be payan resondi')
-        if chbr == 0:
-                print('vayyyy! voroodi 0 dadi alan esmo vared mikoni ke chi besheee!')        
+                print('You have finished the lottery')
                 break
+        
         plrname_list.append(plrname)
+
+        for plrname in plrname_list:
+                 plrname_list.remove(plrname)
+                 print('iin name ghablan boode')
+    
         if plrname == '':
                plrname_list.remove('')
-               print('voroodi nemitoone khali bashe!')
+               print('input cannot be empty!')
 
         print(plrname_list)
 
 for i in range(chbr):
     chplr = random.choice(plrname_list)
-    print(f'barande nafare {i+1} ghore: {chplr}')
+    print(f'Winner of lottery number {i+1}: {chplr}')
+
+
 
 
 
