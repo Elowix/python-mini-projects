36be496f0a18d23a4a2e72468b4b86ac97f31157
𝐄𝐫𝐟𝐚𝐧 <127898295+Erfan-0@users.noreply.github.com>
2024-12-10 10:38:29 +0330

    Update cookie-clicker.py

--- python-mini-projects/auto-website-clicker/cookie-clicker.py
+++ python-mini-projects/auto-website-clicker/cookie-clicker.py
@@ -5,26 +5,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 
-
 chrome_driver_path = "./chromedriver-linux64/chromedriver"
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service)
 
-try:
-    driver.get('https://orteil.dashnet.org/cookieclicker/')
 
-    lang_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, 'langSelect-EN'))
-    )
-    lang_button.click()
+driver.get('https://orteil.dashnet.org/cookieclicker/')
 
-    big_cookie = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, 'bigCookie'))
-    )
+lang_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.ID, 'langSelect-EN'))
+)
+lang_button.click()
 
-    for _ in range(100):
-        big_cookie.click()
+big_cookie = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.ID, 'bigCookie'))
+)
 
-    time.sleep(2)  
-finally:
-    driver.quit()
+for _ in range(100):
+    big_cookie.click()
+
+time.sleep(2)  
