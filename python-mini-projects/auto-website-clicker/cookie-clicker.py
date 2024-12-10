5c85e49c03981b64a2a7bb5f1116372ced43a0ad
𝐄𝐫𝐟𝐚𝐧 <127898295+Erfan-0@users.noreply.github.com>
2024-12-10 10:34:24 +0330

    Update cookie-clicker.py

--- python-mini-projects/auto-website-clicker/cookie-clicker.py
+++ python-mini-projects/auto-website-clicker/cookie-clicker.py
@@ -1,28 +1,30 @@
-import time  
-from selenium import webdriver  
-from selenium.webdriver.common.by import By  
-from selenium.webdriver.support.ui import WebDriverWait  
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 
 
-driver = webdriver.Chrome()  
-driver.get('https://orteil.dashnet.org/cookieclicker/') 
+chrome_driver_path = "./chromedriver-linux64/chromedriver"
+service = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=service)
 
+try:
+    driver.get('https://orteil.dashnet.org/cookieclicker/')
 
-element = WebDriverWait(driver, 10).until(  
-    EC.element_to_be_clickable((By.ID, 'langSelect-EN'))  
-)  
-element.click()
+    lang_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, 'langSelect-EN'))
+    )
+    lang_button.click()
 
-time.sleep(5)  
+    big_cookie = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, 'bigCookie'))
+    )
 
-element = WebDriverWait(driver, 7).until(  
-    EC.element_to_be_clickable((By.ID, 'bigCookie'))  
-)  
+    for _ in range(100):
+        big_cookie.click()
 
-for i in range(50):
-    element.click()  
-
-
-time.sleep(2)  
+    time.sleep(2)  
+finally:
+    driver.quit()
