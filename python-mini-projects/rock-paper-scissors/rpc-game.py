219957c126e9f8dc5215427cee051cd887328664
𝐄𝐫𝐟𝐚𝐧 <127898295+Erfan-0@users.noreply.github.com>
2025-01-04 15:03:02 +0330

    Update rpc-game.py

--- python-mini-projects/rock-paper-scissors/rpc-game.py
+++ python-mini-projects/rock-paper-scissors/rpc-game.py
@@ -1,53 +1,3 @@
-# import random
-
-# class Player:
-#     def choose(self):
-#         choice = input('enter your choice(rock/paper/scissors): ').lower
-#         return choice
-
-# class Computer:
-#     def choose(self):
-#         return random.choice(["rock", "paper", "scissors"])
-    
-# class Game:
-#     def conditioanl(self, player_choice, computer_choice):
-#         if player_choice == computer_choice:
-#             return "tied!"
-
-#         elif player_choice == "rock" and computer_choice == "paper":
-#             return "computer win"
-
-#         elif player_choice == "rock" and computer_choice == "scissors":
-#             return "player win"
-
-#         elif player_choice == "paper" and computer_choice == "gheychi":
-#             return "computer win"
-
-#         elif player_choice == "paper" and computer_choice == "rock":
-#             return "player win"
-
-#         elif player_choice == "scissors" and computer_choice == "rock":
-#             return "computer win"
-
-#         elif player_choice == "scissors" and computer_choice == "paper":
-#             return "computer win"
-
-
-#     def start(self):
-#         print('game started!')
-#         player_choice = self.player.choose()
-#         computer_choice = self.computer.choose()
-#         print(f"computer choose: {computer_choice}")
-#         result = self.conditioanl(player_choice, computer_choice)
-#         print(result)
-
-
-# if __name__ == "__main__":  
-#     game = Game()  
-#     game.player = Player()  
-#     game.computer = Computer()  
-#     game.start()  
-
 
 
 import random  
@@ -109,6 +59,95 @@ elif user_joon < pc_joon:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# we have bug here
+
+# import random
+
+# class Player:
+#     def choose(self):
+#         choice = input('enter your choice(rock/paper/scissors): ').lower
+#         return choice
+
+# class Computer:
+#     def choose(self):
+#         return random.choice(["rock", "paper", "scissors"])
+    
+# class Game:
+#     def conditioanl(self, player_choice, computer_choice):
+#         if player_choice == computer_choice:
+#             return "tied!"
+
+#         elif player_choice == "rock" and computer_choice == "paper":
+#             return "computer win"
+
+#         elif player_choice == "rock" and computer_choice == "scissors":
+#             return "player win"
+
+#         elif player_choice == "paper" and computer_choice == "gheychi":
+#             return "computer win"
+
+#         elif player_choice == "paper" and computer_choice == "rock":
+#             return "player win"
+
+#         elif player_choice == "scissors" and computer_choice == "rock":
+#             return "computer win"
+
+#         elif player_choice == "scissors" and computer_choice == "paper":
+#             return "computer win"
+
+
+#     def start(self):
+#         print('game started!')
+#         player_choice = self.player.choose()
+#         computer_choice = self.computer.choose()
+#         print(f"computer choose: {computer_choice}")
+#         result = self.conditioanl(player_choice, computer_choice)
+#         print(result)
+
+
+# if __name__ == "__main__":  
+#     game = Game()  
+#     game.player = Player()  
+#     game.computer = Computer()  
+#     game.start()  
 
 
 
